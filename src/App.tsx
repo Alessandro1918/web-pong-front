68894b964adda4a7041445bf46eb34ace24ce6b4
Alessandro1918 <alessandro1918@hotmail.com>
2024-04-02 21:37:20 -0300

    feat: countdown after player scores reset game looping

--- src/App.tsx
+++ src/App.tsx
@@ -45,15 +45,18 @@ function App() {
     p2: true
   })
 
-  const [ball, setBall] = useState<RectangleProps>({
+  const [ countdown, setCountdown ] = useState(3)
+
+  const initialBall = {
     x: 400,
-    y: 100,
+    y: 200,
     dx: 0,
     dy: 0,
     width: screen.height / 10,
     height: screen.height / 10,
     color: "orange"
-  })
+  }
+  const [ball, setBall] = useState<RectangleProps>(initialBall)
 
   const [p1, setP1] = useState<RectangleProps>({
     x: 0,
@@ -103,20 +106,18 @@ function App() {
     color: "yellow"
   }
 
-  //At "Play", init ball with random speed and direction
+  //At "Play", start countdown. At zero, start ball
   useEffect(() => {
-    const rx = Math.random() > 0.5 ? 30 : -30     //Or 20, or -20 (don't go straight up/down)
-    const ry = Math.random() > 0.5
-      ? (Math.random() * (50 - 5) + 5)            //Or between 50 to 5,
-      : (Math.random() * ((-5) - (-50)) + (-5))   //or between -5 to -50 (don't go straight left/right)
     if (!isPaused) {
-      setBall({
-        ...ball,
-        dx: rx,
-        dy: ry
-      })
+      const timer = setInterval(() => {
+        setCountdown(countdown - 1)
+        if (countdown == 1) {
+          startBall()
+        }
+      }, 1000)  //1s
+      return () => clearInterval(timer)
     }
-  }, [isPaused])
+  }, [countdown, isPaused])
 
   //Each time the ball moves, wait a time interval and change the ball position again
   useEffect(() => {
@@ -135,21 +136,45 @@ function App() {
 
     //P1 scored
     if (isColliding(ball, rightWall)) {
-      setIsPaused(true)
-      //TODO: score, reset ball
+      setScore({...score, p1: score.p1 + 1})
     }
 
     //P2 scored
     if (isColliding(ball, leftWall)) {
-      setIsPaused(true)
-      //TODO: score, reset ball
+      setScore({...score, p2: score.p2 + 1})
     }
 
+    //Move ball
     const interval = setInterval(() => {
       moveBall(ball.dx!, ball.dy!)
     }, 150) //ms
     return () => clearInterval(interval)
   }, [ball, isPaused])
+
+  //At every score: reset ball and countdown
+  useEffect(() => {
+    setBall(initialBall)
+    setCountdown(3)
+  }, [score])
+
+  //Init ball with random speed and direction
+  function startBall() {
+    if (
+      !isPaused && 
+      ball.dx! == 0 &&
+      ball.dy! == 0
+    ) {
+      const rx = Math.random() > 0.5 ? 30 : -30     //Or 30, or -30 (don't go straight up/down)
+      const ry = Math.random() > 0.5
+        ? (Math.random() * (50 - 5) + 5)            //Or between 50 to 5,
+        : (Math.random() * ((-5) - (-50)) + (-5))   //or between -5 to -50 (don't go straight left/right)
+      setBall({
+        ...ball,
+        dx: rx,
+        dy: ry
+      })
+    }
+  }
 
   function moveBall(dx: number, dy: number) {
     if (!isPaused) {
@@ -169,8 +194,8 @@ function App() {
     }
   }
 
+  //https://www.jeffreythompson.org/collision-detection/rect-rect.php
   function isColliding(obj1: RectangleProps, obj2: RectangleProps) {
-    //https://www.jeffreythompson.org/collision-detection/rect-rect.php
     return (
       obj1.x + obj1.width >= obj2.x &&    // rect1 right edge past rect2 left
       obj1.x <= obj2.x + obj2.width &&    // r1 left edge past r2 right
@@ -190,16 +215,19 @@ function App() {
 
   return (
     <>
-      {/* Header */}
       <div className="header">
-        <h1>{
-          `${score.p1} ${space(10)} ${score.p2}`}
-        </h1>
-        <h3>{
-          `${connectionStatus.p1 ? space(21) : "[OFFLINE]"}
-           ${space(10)}
-           ${connectionStatus.p2 ? space(21) : "[OFFLINE]"}`}
+        <h1>{`${score.p1} ${space(10)} ${score.p2}`}</h1>
+
+        <h3>
+          {`${connectionStatus.p1 ? space(21) : "[OFFLINE]"}
+            ${space(10)}
+            ${connectionStatus.p2 ? space(21) : "[OFFLINE]"}`}
         </h3>
+
+        {
+          countdown > 0 &&
+          <h1>{`${space(1)} ${countdown}`}</h1>
+        }
       </div>
 
       <div className="vertical_dotted_line"></div>
@@ -280,11 +308,6 @@ function App() {
         height: rightWall.height, 
         background: rightWall.color
       }}></div>
-
-      <button onClick={() => moveBall(0, -10)}>Up</button>
-      <button onClick={() => moveBall(0, 10)}>Down</button>
-      <button onClick={() => moveBall(-10, 0)}>Left</button>
-      <button onClick={() => moveBall(10, 0)}>Right</button>
 
       <button onClick={() => setIsPaused(!isPaused)}>Play/Pause</button>
 
